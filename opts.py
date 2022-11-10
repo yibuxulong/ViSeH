c6e2d0e94876f0a7d00b0d9657b4f82a05857aba
forCVPRreview <xiangxian_lee@mail.sdu.edu.cn>
2022-11-10 13:39:55 +0800

    Update opts.py

--- opts.py
+++ opts.py
@@ -2,6 +2,7 @@ import argparse
 
 def opt_algorithm():
     parser = argparse.ArgumentParser()
+    # dataset
     parser.add_argument('--dataset', type=str, default= 'food101',help='indicator to dataset')
     # path setting
     parser.add_argument('--result_path', type=str, default= 'result/',help='path to the folder to save results')
@@ -12,7 +13,7 @@ def opt_algorithm():
     parser.add_argument('--net_s', type=str, default= 'gru',help='choose network backbone for ingredient channel')
     parser.add_argument('--method', type=str, default= 'global',help='to chose model and dataset')    
     
-    # turning parameters
+    # common parameters
     
     parser.add_argument('--batch_size', type=int, default = 64, help='batch size')
     parser.add_argument('--lr', type=float, default = 5e-4, help='learning rate')
@@ -20,24 +21,24 @@ def opt_algorithm():
     parser.add_argument('--lr_decay', type=int, default = 4, help='decay rate of learning rate')
     parser.add_argument('--weight_decay', type=float, default = 1e-3, help='weight decay')
 
+    parser.add_argument('--w_semantic', type=float, default = 5, help='weight of semantic')
+    parser.add_argument('--w_visual', type=float, default = 5, help='weight of visual')
+    
+    # FVSA param
+    parser.add_argument('--tsu', type=float, default = 0.8, help='theta of scale upperbound')
+    parser.add_argument('--tsl', type=float, default = 0.8, help='theta of scale lowerbound')
+    parser.add_argument('--wul', type=float, default = 1., help='weight of upper loss')
+    parser.add_argument('--wll', type=float, default = 1., help='weight of lower loss')
+    parser.add_argument('--wdl', type=float, default = 5., help='weight of diverse loss')
+    parser.add_argument('--waols', type=float, default = 100., help='weight of anti outlier loss shift')
+    parser.add_argument('--waolb', type=float, default = 100., help='weight of anti outlier loss bounds')
+    parser.add_argument('--wrl', type=float, default = 0., help='weight of rotate loss')
+    
     parser.add_argument('--topk', type=int, default = 9,help='top-k classes')
     parser.add_argument('--top_cls', type=int, default = 1,help='top cls for hierarchy')
     parser.add_argument('--top_pos', type=int, default = 5,help='top pos in each seq')
     parser.add_argument('--top_seq', type=int, default = 9,help='top seq, default: the mean of words in dataset')
-    
-    parser.add_argument('--w_semantic', type=float, default = 5, help='weight of semantic')
-    parser.add_argument('--w_visual', type=float, default = 5, help='weight of visual')
-    
-    # STN param
-    parser.add_argument('--tsu', type=float, default = 0.8, help='coefficient of  l2norm loss between image features and word features')
-    parser.add_argument('--tsl', type=float, default = 0.8, help='coefficient of  l2norm loss between image features and word features')
-    parser.add_argument('--wul', type=float, default = 1., help='coefficient of  l2norm loss between image features and word features')
-    parser.add_argument('--wll', type=float, default = 1., help='coefficient of  l2norm loss between image features and word features')
-    parser.add_argument('--wdl', type=float, default = 5., help='coefficient of  l2norm loss between image features and word features')
-    parser.add_argument('--waols', type=float, default = 100., help='coefficient of  l2norm loss between image features and word features')
-    parser.add_argument('--waolb', type=float, default = 100., help='coefficient of  l2norm loss between image features and word features')
-    parser.add_argument('--wrl', type=float, default = 0., help='coefficient of  l2norm loss between image features and word features')
-    
+
     # VSHC param
     parser.add_argument('--beta_know', type=float, default = 0.1, help='weight of relation refine')
     
